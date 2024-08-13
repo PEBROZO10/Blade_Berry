da53a6d596ea26ccd9d978f0f19f4eb9099f062f
PEBROZO10 <88169167+PEBROZO10@users.noreply.github.com>
2024-08-13 13:49:17 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,23 +1,3 @@
-// Armazenar histórico de rolagens
-let history = [];
-
-// Atualizar histórico
-function updateHistory(result) {
-    history.push(result);
-    if (history.length > 3) {
-        history.shift(); // Remove o item mais antigo se houver mais de 3 itens
-    }
-
-    const historyList = document.getElementById('history-list');
-    historyList.innerHTML = ''; // Limpar a lista atual
-
-    history.forEach((item, index) => {
-        const li = document.createElement('li');
-        li.textContent = `Rolagem ${index + 1}: ${item}`;
-        historyList.appendChild(li);
-    });
-}
-
 // Rolagem de Dados
 document.getElementById('dice-form').addEventListener('submit', function(event) {
     event.preventDefault();
@@ -32,7 +12,6 @@ document.getElementById('dice-form').addEventListener('submit', function(event) 
     const result = Math.floor(Math.random() * sides) + 1;
     
     document.getElementById('dice-result').textContent = `Resultado: ${result}`;
-    updateHistory(result);
     
     // Limpar o formulário
     this.reset();
